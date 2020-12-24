c1229813b33a8664cbef72a7f39172290df7bec0
ahmed <ahmedazmycs@protonmail.com>
2020-12-24 15:55:04 +0200

    second app

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 const search = document.querySelector('#search');
 const matchList = document.querySelector('#match-list');
+const toggleButton = document.querySelector('.toggle');
 
+
+// get the word list data
+initializeData = async () => { 
+    result = await fetch('data/words_dictionary.json');
+    data = await result.json();
+    words_list = Object.entries(data);
+}
+initializeData();
 
 // flesh up the UI with autocomplete matches 
 const fleshUp = (matches) => {
     if ( matches.length > 0 ) {
-        const html = '<h6 class="m-1">Top 10 matches</h6>' + matches.slice(0,10).map( match => 
+        const html = matches.slice(0,3).map( match => 
                 `
-                    <div class='card card-body mb-2'>
-                        <p class='result text-primary my-auto' style='font-family:Monaco;cursor: pointer;'>${match[0]}</p>
+                <div class="col-sm">
+                    <div class='card card-body py-2'>
+                        <p class='result text-primary my-1 ' style='font-family:Monaco;cursor:pointer;'>${match[0]}</p>
                     </div>
+                </div>
                 `
             ).join('');
-        matchList.innerHTML=html;
+        matchList.innerHTML= `<div class='row'>${html}</div>`;
     } else { 
         matchList.innerHTML =  `
-            <div class='card card-body mb-1'>
+            <div class='card card-body mb-0'>
                 <p class='text-primary my-auto' style='font-family:Monaco;'> </p>
             </div>
         `;
@@ -25,10 +36,8 @@ const fleshUp = (matches) => {
 
 // Search Words List and filter it
 const searchWords = async (input) => {
+
     let searchText = input.trim().toLowerCase();
-    const result = await fetch('data/words_dictionary.json');
-    const data = await result.json();
-    const words_list = Object.entries(data);
    
     //get matches to current text input
     const regex = new RegExp(`^${searchText}`)
@@ -42,12 +51,38 @@ const searchWords = async (input) => {
     fleshUp(matches);
 }
 
+//gets last word of text input
+const lastWord = ( input ) => {
+    let words = input.split(' ');
+    return words[words.length-1];
+}
 
-search.addEventListener('input',()=> searchWords(search.value))
-
-matchList.addEventListener('click', e => {
+//replace input last word with selected match
+const replaceLastWord = (e) => {
     if ( e.target.classList.contains('result')) {
-        search.value = e.target.innerText;
-        fleshUp([]);
+       let words = search.value.split(' ');
+       words[words.length-1] = e.target.innerText;
+       search.value = words.join(' ')+' ';
+       fleshUp([]);
+       search.focus();
     }
-})
+}
+
+//toggle theme color
+let theme = 'light';
+const toggleTheme = () => {
+    if ( theme === 'light') {
+        document.querySelector('.theme').href = '/css/dark.css';
+        theme = 'dark';
+    }
+   else { 
+        document.querySelector('.theme').href = '/css/light.css';
+        theme = 'light';
+   }
+    
+}
+
+search.addEventListener('input',()=> searchWords(lastWord(search.value)));
+matchList.addEventListener('click', e => replaceLastWord(e));
+toggleButton.addEventListener('click', () => toggleTheme())
+
